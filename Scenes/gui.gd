af0e7320dc68d0c1880d27adf2157c8306b8e53c
MEH4231 <origami.flynn@gmail.com>
2024-04-02 17:06:21 +1100

    Working with history sorta

--- Scenes/gui.gd
+++ Scenes/gui.gd
@@ -1,9 +1,14 @@
 extends Control
 
 var SongList = []
+var Shuffle = false
+var PlayingSong = []
+
+var PreviousSongs = []
 
 func _ready():
-	GetDirectories("C:/Users/XLR8/Music")
+	#GetDirectories("C:/Users/XLR8/Music")
+	GetDirectories("/run/media/kirby/MoreStore/Music/GuiltyGear/")
 	print(SongList.size())
 	for Song in SongList:
 		$SongList.add_item(Song[0])
@@ -13,6 +18,7 @@ func _process(delta):
 	
 	var time = snapped($SongPlayer.get_playback_position(),1)
 	var hours = snapped((int(time) / 60) / 60,1)
+	time -= hours * 60 * 60
 	var mins = snapped(int(time) / 60,1)
 	time -= mins * 60
 	var secs = snapped(int(time),1)
@@ -28,16 +34,19 @@ func GetDirectories(Path: String):
 		for File in Dir.get_files():
 			#print(File)
 			if File.ends_with(".mp3"):
-				SongList.append([File, Path + "/" + File, "mp3"])
+				SongList.append([File, Path + "/" + File, "mp3", SongList.size()])
 			elif File.ends_with(".flac"):
-				SongList.append([File, Path + "/" + File, "flac"])
+				SongList.append([File, Path + "/" + File, "flac", SongList.size()])
 
 
 func _on_song_list_item_activated(index):
 	LoadSong(index)
 
-func LoadSong(SongNumber):
+func LoadSong(SongNumber: int):
+	if SongNumber > SongList.size() - 1 or SongNumber < 0:
+		SongNumber = 0
 	var SelectedSong = SongList[SongNumber]
+	PlayingSong = SongList[SongNumber]
 	
 	var MusicStream
 	if SelectedSong[2] == "mp3":
@@ -53,8 +62,22 @@ func LoadSong(SongNumber):
 		return
 	$SongPlayer.stream = MusicStream
 	$SongPlayer.play()
+	if PreviousSongs.size() > 0:
+		if PreviousSongs[0] == SelectedSong:
+			PreviousSongs.remove_at(0)
+		#else:
+			#PreviousSongs.insert(0,SelectedSong)
+	#else:
+	PreviousSongs.insert(0,SelectedSong)
+	if PreviousSongs.size() > 100:
+		PreviousSongs.remove_at(100)
+	$History.clear()
+	for Song in PreviousSongs:
+		$History.add_item(Song[0])
 	
 	$Controls/Buttons/Container/Play.texture_normal = ResourceLoader.load("res://Textures/Pause.png")
+	$SongList.select(SongNumber)
+	$SongList.ensure_current_is_visible()
 	$Controls/SongName.text = SelectedSong[0]
 	$Controls/TimeLeft.max_value = MusicStream.get_length()
 	var time = snapped(MusicStream.get_length(),1)
@@ -65,10 +88,18 @@ func LoadSong(SongNumber):
 	$Controls/TimeLeft/Length.text = (str(hours) + "h " + str(mins) + "m " + str(secs) + "s")
 
 
-func _on_slider_gui_input(_event):
-	$SongPlayer.volume_db = ($Controls/Slider.value -100)
-	$Controls/Slider/Percent.text = str(($Controls/Slider.value)) + "%"
+func _on_volume_slider_gui_input(_event):
+	$SongPlayer.volume_db = ($Controls/VolumeSlider.value -100)
+	$Controls/VolumeSlider/Percent.text = str(($Controls/VolumeSlider.value)) + "%"
 
+
+func _on_song_player_finished():
+	if Shuffle == true:
+		var random = RandomNumberGenerator.new()
+		random.randomize()
+		LoadSong(random.randi_range(1,SongList.size()) - 1)
+	else:
+		LoadSong($SongList.get_selected_items()[0] + 1)
 
 func _on_play_pressed():
 	if $SongPlayer.stream.get_length() > 0:
@@ -77,22 +108,58 @@ func _on_play_pressed():
 			$Controls/Buttons/Container/Play.texture_normal = ResourceLoader.load("res://Textures/Pause.png")
 		elif $SongPlayer.playing == false:
 			$Controls/Buttons/Container/Play.texture_normal = ResourceLoader.load("res://Textures/Play.png")
+	else:
+		$Controls/Buttons/Container/Play.texture_normal = ResourceLoader.load("res://Textures/Pause.png")
+		if Shuffle == true:
+			var random = RandomNumberGenerator.new()
+			random.randomize()
+			LoadSong(random.randi_range(1,SongList.size()) - 1)
+		else:
+			LoadSong(0)
 
 
 func _on_stop_pressed():
 	$SongPlayer.stop()
+	$SongPlayer.stream = AudioStream.new()
 	$Controls/SongName.text = "None playing"
 	$Controls/Buttons/Container/Play.texture_normal = ResourceLoader.load("res://Textures/Play.png")
 	$Controls/TimeLeft/Length.text = "0h 0m 0s"
 
 
-func _on_song_player_finished():
-	var random = RandomNumberGenerator.new()
-	random.randomize()
-	LoadSong(random.randi_range(1,SongList.size()) - 1)
-
-
 func _on_next_pressed():
-	var random = RandomNumberGenerator.new()
-	random.randomize()
-	LoadSong(random.randi_range(1,SongList.size()) - 1)
+	if $SongPlayer.stream.get_length() > 0:
+		if Shuffle == true:
+			var random = RandomNumberGenerator.new()
+			random.randomize()
+			LoadSong(random.randi_range(1,SongList.size()) - 1)
+		else:
+			LoadSong(PlayingSong[3] + 1)
+	else:
+		if Shuffle == true:
+			var random = RandomNumberGenerator.new()
+			random.randomize()
+			LoadSong(random.randi_range(1,SongList.size()) - 1)
+		else:
+			LoadSong(0)
+
+
+func _on_previous_pressed():
+	if PreviousSongs.size() > 0:
+		LoadSong(PreviousSongs[1][3])
+	else:
+		LoadSong(PlayingSong[3] - 1)
+
+
+func _on_forward_pressed():
+	$SongPlayer.seek($SongPlayer.get_playback_position() + 10)
+
+
+func _on_back_pressed():
+	if $SongPlayer.get_playback_position() - 10 < 0:
+		$SongPlayer.seek(0)
+	else:
+		$SongPlayer.seek($SongPlayer.get_playback_position() - 10)
+
+
+func _on_shuffle_toggled(toggled_on):
+	Shuffle = toggled_on
